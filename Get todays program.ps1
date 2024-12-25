fbb96b86d9bf66b38daf07d76e99f6290b480d99
jpplaisier <41903502+jpplaisier@users.noreply.github.com>
2024-12-25 15:54:03 +0100

    Update Get todays program.ps1

--- Get todays program.ps1
+++ Get todays program.ps1
@@ -241,8 +241,7 @@ $htmlContent = @"
     <div class="snowflake" style="left: 90%;">❆</div>        
 
     <header>
-        <img src='https://cdn.prod.website-files.com/61c2f086d385db179866da52/61c2ff8084dad62e03fa7111_HWPO-Training-Logo-White.svg' alt='HWPO Logo'>
-        <img src='https://r2-us-west.photoai.com/1714366698-e9536ed358cacbd2481b0818e4d4e9b1-2.png' alt='BiSanta'>
+        <img src='https://cdn.prod.website-files.com/61c2f086d385db179866da52/61c2ff8084dad62e03fa7111_HWPO-Training-Logo-White.svg' alt='
         <h1>🎄 Weekly Training Schedule 🎄</h1>
         <div class="day-selector">
             <button onclick="showDay(1)" id="button-1">Monday</button>
